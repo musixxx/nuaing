e7995f7c861a9c3dcdf62361d11204844031f708
fadlulazmi <fadlul_azmi@yahoo.com>
2019-07-11 21:42:01 +0700

    done lyric and media

--- server/models/user.js
+++ server/models/user.js
@@ -1,20 +1,53 @@
-var mongoose = require('mongoose');
-const bcrypt = require('../helpers/bcrypt')
-var Schema = mongoose.Schema;
+const mongoose = require('mongoose');
+const {Schema} = mongoose;
+const Helper = require('../helpers/helper')
 
-var userSchema = new Schema({
+const userSchema = new Schema({
     username: {
         type: String,
         required: true
     },
-    password: String,
-    email: String
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        validate: [
+            {
+                validator: function(value) {
+                    if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(value)) {
+                        throw 'Invalid email format'
+                    }
+                },
+            },
+            {
+                validator: function(value) {
+                    return User.find({
+                        _id: { $ne: this._id },
+                        email: value
+                    })
+                    .then( data => {
+                        if(data.length !== 0) {
+                            throw 'Email has been registered';
+                        }
+                    })
+                    .catch(err => {
+                        throw err;
+                    });
+                }
+            }
+        ]
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: [4, 'Password min length is 4'],
+    }
 });
 
-userSchema.pre('save', function (next) {
-    this.password = bcrypt.hashPassword(this.password)
+userSchema.pre('save', function(next, done) {
+    this.password = Helper.hashPassword(this.password)
     next()
-})
+});
 
 const User = mongoose.model('User', userSchema);
 
--- server/routes/APIroute.js
+++ server/routes/APIroute.js
@@ -6,7 +6,12 @@ const LyricController = require('../controllers/lyricController')
 
 router.get('/events', EventController.getEvent)
 router.get('/events/search', EventController.searchEvent)
+
 router.get('/media', MediaController.searchMedia)
+
 router.get('/lyrics', LyricController.searchLyric)
+router.get('/lyrics/tracks', LyricController.searchTrackId)
+
+
 
 module.exports = router
