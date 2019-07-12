c2e54f30924627fbc9cd7c40fdcaad49a40f8831
irshadi <bagasputro.irshadi@gmail.com>
2019-07-12 09:59:55 +0700

    login-register page jadi

---
+++ client/js/getstart.js
@@ -0,0 +1,14 @@
+if (localStorage.token) {
+  $("#getting-started").hide();
+  $("#home").show();
+}
+
+$("#btn-register").click(function(){
+  $("#form-register").show();
+  $("#form-signin").hide();
+});
+
+$("#btn-login").click(function(){
+  $("#form-signin").show();
+  $("#form-register").hide();
+});
