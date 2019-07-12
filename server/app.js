8486cf77e7c880ce612438fd860c8f10a0e92128
irshadi <bagasputro.irshadi@gmail.com>
2019-07-12 09:25:10 +0700

    API songkick done

--- server/app.js
+++ server/app.js
@@ -7,8 +7,8 @@ const route = require('./routes')
 const mongoose = require('mongoose')
 const cors = require('cors')
 const app = express()
-const port = process.env.PORT || 3000
-
+const port = process.env.PORT
+const morgan = require('morgan');
 
 app.use(cors())
 app.use(express.urlencoded({ extended:false }))
