dd17f3918e9b634d6c870c6db95329060855c1a1
basyri <husain.basyri@gmail.com>
2019-07-12 15:04:52 +0700

    change client

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const APIroutes = require('./APIroute')
-const users = require('././user')
+const users = require('./user')
 
-router.use('/api', APIroutes)
 router.use('/users', users)
+router.use('/api', APIroutes)
+
+// router.use('/users', users)
 
 module.exports = router
