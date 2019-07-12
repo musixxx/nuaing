27cc1d4bf92237c11a57c206e2a990617e1d62e6
fadlulazmi <fadlul_azmi@yahoo.com>
2019-07-12 14:45:15 +0700

    tracklist ready

--- server/controllers/mediaController.js
+++ server/controllers/mediaController.js
@@ -2,14 +2,41 @@ const axios = require('axios')
 
 class MediaController {
 
-    static async searchMedia(req, res){
+    static async searchMedia(req, res, next){
         try {
             console.log(req.query.q);
-            let {data} = await axios({
+            let response = await axios({
                 url : `https://api.deezer.com/search?q="${req.query.q}"`
             })
-            console.log('data: ', data);
-            res.status(200).json(data.data[0].artist)
+            console.log('data: ', response.data.data[0].artist);
+            let result = {}
+            
+            let details = response.data.data[0].artist
+            result.detail = details
+
+            let tracks = await axios({
+                url : `https://api.deezer.com/artist/${details.id}/top?limit=5%index=5`,
+                method : 'GET'
+            })
+            
+            // console.log(tracks.data);
+            let arr = []
+
+            let arrayTracks = tracks.data.data
+            console.log('arrayTracks: ', arrayTracks);
+
+            
+
+            arrayTracks.forEach(element => {
+                arr.push(element.preview)
+            });
+
+            console.log(arr);
+
+            result.tracklist = arr
+
+            res.status(200).json(result)
+
 
         } catch(err){
             next(err)
