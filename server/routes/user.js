f1be26739d71ceb80bd8df8b847b01739920613d
fadlulazmi <fadlul_azmi@yahoo.com>
2019-07-12 10:56:39 +0700

    solve signin signup user

--- server/routes/user.js
+++ server/routes/user.js
@@ -4,5 +4,6 @@ const UserController = require('../controllers/userController')
 
 Router.post('/signup', UserController.signup)
 Router.post('/signin', UserController.signin)
+Router.post('/g-sigin', UserController.googleSignin)
 
 module.exports = Router
